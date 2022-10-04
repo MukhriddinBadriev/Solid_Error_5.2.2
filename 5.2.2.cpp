fdead3f6e326f5d833b89ba35f1c2db47bcc2b56
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-10-04 23:57:00 +0300

    Initial commit

---
+++ 5.2.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int sum(int a, int b) {
+    return a * b;
+}
+
+int minus(int, int);
+
+int main()
+{
+    cout << "Hello World!\n";   //синтаксическая ошибка (надо написать std::cout)
+
+    int a = 5;
+    int b = 2;
+
+    std::cout << sum(a, b);  //семантическая ошибка (функция sum должна вернуть (a + b) )
+
+    std::cout << minus(a, b);  // ошибка линковки  (надо написать определения функции minus)
+
+    return 0;
+}
